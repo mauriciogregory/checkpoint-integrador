5527b08c20797aeb3242049a10e6d59a87a98b8d
mauriciogregory <mauricio.gre@gmail.com>
2022-04-08 23:21:46 -0300

    add admin page

--- frontend/src/pages/Cart/index.jsx
+++ frontend/src/pages/Cart/index.jsx
@@ -1,6 +1,9 @@
+import Header from '../../components/header'
+
 export default function Cart() {
     return (
         <div>
+
             <h1>Aqui será o Carrinho</h1>
         </div>
     )
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "../pages/Home/index"
 import Erro404 from "../pages/404/index"
 import Cart from "../pages/Cart/index"
+import Admin from "../pages/Admin"
 
 export default function Rotas() {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                {/* <Route path="" element={} /> */}
+                <Route path="/admin" element={<Admin />} />
                 <Route path="*" element={<Erro404 />} />
                 <Route path="/cart" element={<Cart />} />
             </Routes>
