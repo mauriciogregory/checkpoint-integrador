1a1c079633a67004e7b72bc385e959bcfc300e19
viniTWL <vinigdc12@gmail.com>
2022-04-12 17:46:20 -0300

    creating useful hooks

---
+++ frontend/src/hooks/useUtil.js
@@ -0,0 +1,22 @@
+export function formatPrice(value) {
+    if (value) {
+      return new Intl.NumberFormat("pt-BR", { // classe Javascript
+        style: "currency", // atributos da formatação
+        currency: "BRL", 
+      }).format(Number(value)); // data formatada
+    }
+        return 'Saldo Indisponivel' // trataiva
+    }
+    
+    // formatação de data
+    
+    export function formatData(dateUTC){
+        if (dateUTC) {
+            return new Intl.DateTimeFormat("pt-BR", { 
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit"
+            }).format(new Date(dateUTC))
+        }
+            return 'Data não informada'
+    }
