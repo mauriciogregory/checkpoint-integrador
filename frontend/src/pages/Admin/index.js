b0f50068534e890f452a18a0083d5b3742a3419a
mauriciogregory <mauricio.gre@gmail.com>
2022-04-19 17:48:43 -0300

    Admin method delete

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -16,56 +16,82 @@ export default function Admin() {
 
   useEffect(() => {
     getapiData();
+    // deleteData();
   }, []);
 
+  // get
   const getapiData = async () => {
     const { data } = await api.get("/products?&size=1000");
     setProducts(data.content);
   };
 
-  const deleteData = async ({id}) => {
-    let a = await api.delete("/products/"+id)
-    console.log(a)
-  }
+  // delete
+  // async function deleteData(id) {
+  //   await api.delete(`/products/${id}`)
+  //   console.log(id)
+  // }
+
+  //put
+  const newProduct = {
+    title: "Novo",
+    description: "novo",
+    price: 23.33,
+    image: "",
+    categories: [],
+  };
+
+  //post
+  // function addNewProduct() {
+  //   api
+  //     .post("/products/", newProduct)
+  //     .then((res) => {
+  //       res.data;
+  //     })
+  //     .catch((error) => console.log(error));
+  // }
 
   return (
     <section>
       <Header />
       <div className="container-fluid ">
         <ul className="grid-produtos">
-          {products.map(({id, title, description, price, image}) => {
+          {products.map(({ id, title, description, price, image }) => {
             return (
-                <li key={id} className="li-card">
-                  <ul className="product-list-item">
-                    <li className="card-title">
-                      <strong>{title}</strong>
-                    </li>
-                    <li className="card-description-min">
-                      <p>Descrição do Produto: {description}</p>
+              <li key={id} className="li-card">
+                <ul className="product-list-item">
+                  <li className="card-title">
+                    <strong>{title}</strong>
+                  </li>
+                  <li className="card-description-min">
+                    <p>Descrição do Produto: {description}</p>
+                  </li>
+                  <li>
+                    <img
+                      src={image}
+                      className="card-img"
+                      variant="top"
+                      alt="imagem"
+                    />
+                  </li>
+                  <li className="">
+                    <p>Preço: {formatPrice(price)}</p>
+                  </li>
+                  <div className="botoes">
+                    <li>
+                      <button className="button-product-list">Editar</button>
                     </li>
                     <li>
-                      <img
-                        src={image}
-                        className="card-img"
-                        variant="top"
-                      />
+                      {/* <Link className="unstyle"  to={(`/products/${id}`)}> */}
+                      <button
+                        className="button-product-list"
+                      >
+                        Excluir
+                      </button>
+                      {/* </Link> */}
                     </li>
-                    <li className="">
-                      <p>Preço: {formatPrice(price)}</p>
-                    </li>
-                    <div className="botoes">
-                      <li>
-                      
-                        <button className="button-product-list">Editar</button>
-                      </li>
-                      <li>
-                      <Link className="unstyle"  to={(`/products/${id}`)}>
-                        <button className="button-product-list" onClick={deleteData({id})}>Excluir</button>
-                      </Link>
-                      </li>
-                    </div>
-                  </ul>
-                </li>
+                  </div>
+                </ul>
+              </li>
             );
           })}
         </ul>
