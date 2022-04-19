0eb727e3ac8cc4d83e19a3e9ecfd9063e1c2f203
mauriciogregory <mauricio.gre@gmail.com>
2022-04-18 23:23:42 -0300

    file updated

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,46 +1,54 @@
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 import FooterDown from "../../components/footerdown";
-// import api from '../../service/api';
+import api from '../../service/api';
 import { useEffect, useState } from "react";
-import axios from "axios";
+import "./style.scss";
 
 export default function Admin() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/products").then((resposta) => {
-      setProducts(resposta.data);
-    });
+    getapiData()
   }, []);
+
+
+  const getapiData = async () => {
+      const {data} = await api.get("/products?&size=1000");
+      setProducts(data.content);
+  }
 
   return (
     <section>
       <Header />
-      <div>
-        <ul>
+      <div className="container">
+        <ul className="grid-produtos">
           {products.map((produto) => {
             return (
-              <li key={produto.title}>
-                <ul>
-                  <li>
+              <li key={produto.id} className="li-card">
+                <ul className="product-list-item">
+                  <li className="card-titulo">
                     <strong>{produto.title}</strong>
                   </li>
                   <li>
                     <p>Descrição do Produto: {produto.description}</p>
                   </li>
                   <li>
-                    <p>Preço R$: {produto.price}</p>
+                    <img src={produto.image} className="img-product-list" variant="top"/>
                   </li>
                   <li>
-                    <img src={produto.image} heigth={50} width={100}/>
+                    <p>Preço R$: {produto.price}</p>
                   </li>
+                  <div className="botoes">
+                  <li>
+                    <button className="button-product-list">Editar</button>
+                  </li>
+                  <li>
+                    <button className="button-product-list">Excluir</button>
+                  </li>
+                  </div>
                 </ul>
               </li>
-
-              //   <li key={produto.nome}>
-              //     <strong>{produto.nome}</strong>
-              //   </li>
             );
           })}
         </ul>
